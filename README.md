37bc05190cb849e22226941028b53be67b462daf
dmezh <daniel.mezhiborsky@cooper.edu>
2021-11-08 23:39:03 -0500

    sin ඞ

---
+++ README.md
@@ -0,0 +1,11 @@
+### sus.c
+
+```sh
+gcc sus.c -o sus
+```
+
+```
+./sus
+```
+
+It doesn't do anything bad. ඞ
---
+++ sus.c
@@ -0,0 +1,24 @@
+#define                UU $
+#define              N "\xE0"
+#define              uSusSu s
+#define             ss   open(
+#define             SS    main
+#define             uU   while
+#define             s$s printf
+#define             s __FILE__
+#define             uuu    uu;
+#define             s$$    $,&
+#define             $$$    int
+
+
+
+
+       SS (){$$$    */**/    UU     =ss uSusSu
+       ,0);         char    uuu    /**/
+       uU(          read    (s$$   uu,1
+       )){/**/      /**/    /**/   s$s(uu==
+           '\n'?    "\n"    :uu          ==/**/
+            ' '?    " "     : N         "\xB6"
+         /*UUU*/    /***sus***/    "\236"/**/
+       );}}/**/     /***by***/     /*@dmezh*/
+
